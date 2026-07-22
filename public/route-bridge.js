3db1bd249dfb48e18585cd45721d6931fcdc1b7c
Bardia <iconiccerberus@gmail.com>
2026-07-22 21:03:54 +0330

    Preload hero WebGL model

--- public/route-bridge.js
+++ public/route-bridge.js
@@ -7,14 +7,20 @@ window.__FARA_SOUND_ENABLED__ = false
 window.__FARA_NAVIGATION_MANAGED__ = true
 
 window.setTimeout(() => {
-  const preload = document.createElement('link')
-  preload.rel = 'preload'
-  preload.as = 'fetch'
-  preload.href = '/assets/models/fort-energy/energy-chapter.glb'
-  preload.type = 'model/gltf-binary'
-  preload.crossOrigin = 'anonymous'
-  preload.fetchPriority = 'high'
-  document.head.prepend(preload)
+  const criticalModels = [
+    '/assets/models/fort-energy/energy-chapter.glb',
+    '/assets/models/fort-energy/fort-energy.glb',
+  ]
+  criticalModels.forEach(href => {
+    const preload = document.createElement('link')
+    preload.rel = 'preload'
+    preload.as = 'fetch'
+    preload.href = href
+    preload.type = 'model/gltf-binary'
+    preload.crossOrigin = 'anonymous'
+    preload.fetchPriority = 'high'
+    document.head.prepend(preload)
+  })
 }, 0)
 
 window.addEventListener('click', event => {
