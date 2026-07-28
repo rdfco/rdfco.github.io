8af36f5e00081904cc41d16adcde4e45698b568b
Bardia <iconiccerberus@gmail.com>
2026-07-28 11:31:29 +0330

    Fix solution route page key

--- src/navbar/pages/solution/data.js
+++ src/navbar/pages/solution/data.js
@@ -1,1 +1,1 @@
-export const data = {"key":"AI & Tech","title":"AI & Tech","introduction":"This page is ready for future AI & Tech components and service data.","sections":[]}
+export const data = {"key":"solution","title":"AI & Tech","introduction":"This page is ready for future AI & Tech components and service data.","sections":[]}
